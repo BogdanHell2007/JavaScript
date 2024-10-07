16d075fca09382d0fafee5b97320ca6315ea81d2
BogdanHell2007 <leitanbogdan8@gmail.com>
2024-10-07 22:49:38 +0300

    java

---
+++ IV.Java/BubbleSort.java
@@ -0,0 +1,26 @@
+class BubbleSort{
+
+  public static void main(String[] args) {
+      
+     int temp;
+     int[] array = {1, 5, 2, 4, 3};
+
+     for(int i = 0; i < array.length - 1; i++){
+
+      for(int j = 0; j < array.length - i - 1; j++){
+
+        if(array[j] > array[j + 1]){
+
+          temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] =  temp;
+        }
+      }
+     }
+
+      for(int elements : array){
+
+      System.out.println(elements);
+     }
+  }
+}
