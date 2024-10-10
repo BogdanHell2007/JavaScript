382a96ccd7e8a4a7ec769c49c8b8e9929076fc4c
BogdanHell2007 <leitanbogdan8@gmail.com>
2024-10-10 21:02:46 +0300

    1

--- III.JavaScript/Exercitii/ex.js
+++ III.JavaScript/Exercitii/ex.js
@@ -1,5 +1,11 @@
 // pagina pentru testarea sau gasirea solutiilor intr o problema
 
-let eu = Math.sqrt(6)
+function countdown(n) {
+    if (n < 0) {
+        return; // Cazul de bază - când ajunge la un număr mai mic de 0, funcția se oprește
+    }
+    console.log(n);
+    countdown(n - 1); // Funcția se apelează pe ea însăși cu n-1
+}
 
-console.log(eu)
+countdown(5); 0
